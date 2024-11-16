b46cfbad2ead212c1fe35114e6d3c770a1131cad
Sudashna Amarakoon <n11594268@qut.edu.au>
2024-11-16 17:20:56 +1000

    initial commit

---
+++ Request/__init__.py
@@ -0,0 +1,28 @@
+import logging
+import json
+import azure.functions as func
+from azure.cosmos import exceptions, CosmosClient, PartitionKey
+import os
+
+def main(req: func.HttpRequest, doc:func.DocumentList) -> func.HttpResponse:
+    
+    logging.info('Python HTTP trigger function processed a request.')
+
+    url = os.environ['FUNCTION_URL']
+    key = os.environ['FUNCTION_KEY']
+
+    client = CosmosClient(url, credential=key)
+    DATABASE_NAME = 'resumedb'
+    database = client.get_database_client(DATABASE_NAME)
+    CONTAINER_NAME = 'Visitors'
+    container = database.get_container_client(CONTAINER_NAME)
+    documentid = "1"
+
+    read_item = container.read_item(item=documentid, partition_key=documentid)
+    count = read_item['count'] + 1
+    read_item['count'] = count
+    container.replace_item(item=read_item, body=read_item)
+
+    return func.HttpResponse(
+            f"Visitor count updated to: {count}"   
+    ) 
